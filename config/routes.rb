ab262cacb7240be88e040cecc413c0f3b7a216a6
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-09-04 05:01:42 -0400

    commit tree c92430d1cc25f096d658a138407a5caffeb7aa44

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  # resources :birds, only: [:index, :show, :create, :update, :destroy]
+  get '/birds', to: 'birds#index'
+  get '/birds/:id', to: 'birds#show'
+  post '/birds', to: 'birds#create'
+
+  patch '/birds/:id', to: 'birds#update'
+  delete '/birds/:id', to: 'birds#destroy'
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
